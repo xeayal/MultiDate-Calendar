0e6567217fa5d0404da8cc8325e6c477c693261d
Khayal Abilhasanov <44834300+xeayal@users.noreply.github.com>
2021-09-14 12:25:08 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,11 @@
 <link rel="stylesheet" href="style.css">
 <!-- script docs -->
 <script src="jquery.min.js"></script>
-
 <!-- start sorce codes -->
 <center>
 <div class="splide">
-	<div class="splide__track">
-		<ul class="splide__list">
+<div class="splide__track">
+<ul class="splide__list">
 <?php
 // current date information
 $month = date('m');
@@ -17,7 +16,6 @@ $day   = date('d');
 
 // calc how many days in a month
 $i = cal_days_in_month(CAL_GREGORIAN, $month, $year); 
-
 // calendar last day is today ($n start from today)
 $n = $day;
 while ($n <= $i) { 
@@ -26,24 +24,22 @@ while ($n <= $i) {
 $currentdate = $year.'-'.$month.'-'.$n; ?>
 <!-- THIS MONTH -->
 <li class="splide__slide day" date='<?=$currentdate ?>'>
-
-		<center>
-			<span style="cursor: pointer;">
+	<center>
+		<span style="cursor: pointer;">
 <?php 
 $wd= date('w', strtotime($currentdate));
- 	 if($wd == 0){echo 'Sun';}
- elseif($wd == 1){echo 'Mon';}
- elseif($wd == 2){echo 'Tue';}
- elseif($wd == 3){echo 'Wed';}
- elseif($wd == 4){echo 'Thu';}
- elseif($wd == 5){echo 'Fri';}
- elseif($wd == 6){echo 'Sat';}
+	 if($wd == 0){echo 'Sun';}
+elseif($wd == 1){echo 'Mon';}
+elseif($wd == 2){echo 'Tue';}
+elseif($wd == 3){echo 'Wed';}
+elseif($wd == 4){echo 'Thu';}
+elseif($wd == 5){echo 'Fri';}
+elseif($wd == 6){echo 'Sat';}
 ?>
 <br>
 <?=$month.'/'.$n ?> <!-- month day -->
-			</span>
-		</center>
-
+		</span>
+	</center>
 </li>
 <?php
 $n += 1;
@@ -60,24 +56,22 @@ $currentdate = $newYear.'-'.$newMonth.'-'.$n;
 <!-- THIS MONTH END -->
 <!-- NEXT MONTH -->
 <li class="splide__slide day" date='<?=$currentdate ?>'>
-
-		<center>
-			<span style="cursor: pointer;">
+	<center>
+		<span style="cursor: pointer;">
 <?php 
 $wd= date('w', strtotime($currentdate));
-     if($wd == 0){echo 'Sun';}
- elseif($wd == 1){echo 'Mon';}
- elseif($wd == 2){echo 'Tue';}
- elseif($wd == 3){echo 'Wed';}
- elseif($wd == 4){echo 'Thu';}
- elseif($wd == 5){echo 'Fri';}
- elseif($wd == 6){echo 'Sat';}
+ if($wd == 0){echo 'Sun';}
+elseif($wd == 1){echo 'Mon';}
+elseif($wd == 2){echo 'Tue';}
+elseif($wd == 3){echo 'Wed';}
+elseif($wd == 4){echo 'Thu';}
+elseif($wd == 5){echo 'Fri';}
+elseif($wd == 6){echo 'Sat';}
 ?>
 <br>
 <?=$newMonth.'/'.$n ?> <!-- month day -->
-			</span>
-		</center>
-	
+		</span>
+	</center>
 </li>
 <?php $n += 1; }?>
 <!-- NEXT MONTH END -->
